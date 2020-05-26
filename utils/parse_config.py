0bd8000b8b7157bd10c63f9297a29531c6ed500f
jason-hu7 <dcjasonhu@gmail.com>
2020-05-26 10:46:37 -0500

    more refactoring on parse_model_config

--- utils/parse_config.py
+++ utils/parse_config.py
@@ -2,21 +2,23 @@ from Typing import List, Dict
 
 def parse_model_config(path: str) -> List[Dict[str, str]]:
     """Parses the yolo-v3 layer configuration file and returns module definitions"""
+    # Read the config file
     with open(path, 'r') as f:
         lines = f.read().split('\n')
     lines = [x.rstrip().lstrip() for x in lines if x and not x.startswith('#')]
+    # Add each block to a list
     module_defs = []
     for line in lines:
         if line.startswith('['): # This marks the start of a new block
             module_defs.append({})
             module_defs[-1]['type'] = line[1:-1].rstrip()
+            # Convolutional layers all need to be specified of batch norm key
             if module_defs[-1]['type'] == 'convolutional':
                 module_defs[-1]['batch_normalize'] = 0
         else:
             key, value = line.split("=")
             value = value.strip()
             module_defs[-1][key.rstrip()] = value.strip()
-
     return module_defs
 
 
