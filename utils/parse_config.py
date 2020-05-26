c2af8413483d184670c513114c0a17607e0344ee
jason-hu7 <dcjasonhu@gmail.com>
2020-05-26 10:34:17 -0500

    refactor parse_model_config

--- utils/parse_config.py
+++ utils/parse_config.py
@@ -1,11 +1,10 @@
+from Typing import List, Dict
 
-
-def parse_model_config(path):
+def parse_model_config(path: str) -> List[Dict[str, str]]:
     """Parses the yolo-v3 layer configuration file and returns module definitions"""
-    file = open(path, 'r')
-    lines = file.read().split('\n')
-    lines = [x for x in lines if x and not x.startswith('#')]
-    lines = [x.rstrip().lstrip() for x in lines] # get rid of fringe whitespaces
+    with open(path, 'r') as f:
+        lines = f.read().split('\n')
+    lines = [x.rstrip().lstrip() for x in lines if x and not x.startswith('#')]
     module_defs = []
     for line in lines:
         if line.startswith('['): # This marks the start of a new block
@@ -19,6 +18,7 @@ def parse_model_config(path):
             module_defs[-1][key.rstrip()] = value.strip()
 
     return module_defs
+
 
 def parse_data_config(path):
     """Parses the data configuration file"""
